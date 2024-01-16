d8352c9605067014e475932aa895ccc0bd5f7f91
Karen Aprigio <karenmoraes222@gmail.com>
2024-01-16 17:48:51 -0300

    aprendendo sobre API

--- Ajax/main.js
+++ Ajax/main.js
@@ -37,3 +37,21 @@ $(document).ready(function() {
         })
     })
 })
+
+//        fetch(endpoint)
+//        .then(function(resposta) {
+//            return resposta.json();
+//        })
+//        .then(function(json) {
+//            const logradouro = resposta.logradouro;
+//            const bairro = resposta.bairro;
+//            const localidade = resposta.localidade; 
+//            const estado = resposta.uf;
+//            const endereco = `${logradouro}, ${bairro} - ${localidade} - ${estado}`;
+//            $('#endereco').val(endereco);
+
+//            setTimeout(function() {
+//                $(botao).find('i').removeClass('d-none');
+//                $(botao).find('span').addClass('d-none');
+//            }, 1000);
+//        })
