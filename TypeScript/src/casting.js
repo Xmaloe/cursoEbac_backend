a1690a2bee965291008b0e12adaa0d0697cd80c1
Karen Aprigio <karenmoraes222@gmail.com>
2024-01-24 08:45:56 -0300

    Remover diretórios

--- TypeScript/src/casting.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var casting;
-(function (casting) {
-    let idade = 25;
-    idade.toFixed();
-})(casting || (casting = {}));
-let nome = 35; // transforma tipo number em string
--- TypeScript/src/casting.ts
+++
@@ -1,7 +0,0 @@
-
-namespace casting {
-    let idade: any = 25;
-    (idade as number).toFixed()
-}
-
-let nome: string = 35 as unknown as string;// transforma tipo number em string
