f908f5daf0c707df72e4d8e579910a2487bc4c21
Karen Aprigio Moraes <karenmoraes222@gmail.com>
2024-05-15 16:46:13 -0300

    revisão do conteúdo

--- ES6/dist/arrow.js
+++ ES6/dist/arrow.js
@@ -1,32 +1,32 @@
-"use strict";
+// "use strict";
 
-var _this = void 0;
+// var _this = void 0;
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Função de seta que retorna a string "Diz Olá"
-var minhaFuncao = function minhaFuncao() {
-  return "Diz Olá";
-};
-// Função de seta que retorna um objeto representando um carro
-var Carro = function Carro() {
-  return {
-    modelo: 'Versa',
-    fabricante: 'Nissan'
-  };
-};
+const minhaFuncao = () => "Diz Olá";
+
+  const Carro = () => ({
+      modelo: 'Versa',
+      fabricante: 'Nissan'
+  })
+
 console.log(minhaFuncao());
 console.log(Carro());
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Objeto representando uma moto
 var Moto = {
   velocidade: 40,
   // Método para acelerar a moto (função regular)
   acelerar: function acelerar() {
-    this.velocidade += 10;
+    this.velocidade += 8;
   },
   // Método para frear a moto (função de seta - com problema)
   frear: function frear() {
     // Problema: Arrow functions não têm seu próprio 'this', 
     // então 'this.velocidade' não funciona conforme esperado aqui.
-    _this.velocidade -= 10;
+    this.velocidade -= 10;
   }
 };
 Moto.acelerar();
