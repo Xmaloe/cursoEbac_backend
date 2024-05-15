52ccc7a95c683b1a838bedfe8fd54d916129041d
Karen Aprigio Moraes <karenmoraes222@gmail.com>
2024-05-15 11:19:43 -0300

    update

--- Ajax/main.js
+++ Ajax/main.js
@@ -1,7 +1,16 @@
+//AJAX - Asynchronous JavaScript and XML (Interação entre back e front sem regarregar a página)
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 //document.addEventListener('DOMContentLoaded', function() {
 //    document.getElementById('buscar').addEventListener('click', function() {
+//        const xhttp = new XMLHttpRequest();
 //        const cep = document.getElementById('cep').value;
 //        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+//        xhttp.open(`GET´, endpoint);
+//        xhttp.send();
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //        fetch(endpoint)
 //            .then(response => {
@@ -13,15 +22,16 @@
 //    });
 //});
 //        })
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 $(document).ready(function() {
     $('#cep').mask('00000-000');
     $('#buscar').click(function() {
-        const cep = $('#cep').val();
+        const cep = $('#cep').val(); //val= receber valor no vazio
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
         const botao = $(this);
         
-        $(this).find('i').addClass('d-none');
+        $(this).find('i').addClass('d-none'); // faz a junção do icone de busca 
         $(this).find('span').removeClass('d-none');
 
         fetch(endpoint)
@@ -54,6 +64,7 @@ $(document).ready(function() {
         }
     })
 });
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //        $.ajax(endpoint).done(function(resposta) {
 //            const logradouro = resposta.logradouro;
@@ -61,7 +72,7 @@ $(document).ready(function() {
 //            const localidade = resposta.localidade; 
 //            const estado = resposta.uf;
 //            const endereco = `${logradouro}, ${bairro} - ${localidade} - ${estado}`;
-//            $('#endereco').val(endereco);
+//            $('#endereco').val(endereco); // valor que será inserido automaticamente no campo 
 
 //            setTimeout(function() {
 //                $(botao).find('i').removeClass('d-none');
@@ -70,5 +81,10 @@ $(document).ready(function() {
 //        })
 //    })
 //})
+
+//O código 'fetch(endpoint).then(function(resposta) { return resposta.json(); })' é como ir à biblioteca pedir um livro específico 
+//(usando 'fetch' com um 'endpoint' é como pedir o livro pelo título). Quando a bibliotecária lhe entrega o livro ('then'), você o abre e começa a 
+//traduzir seu conteúdo de uma língua estrangeira para o seu idioma ('resposta.json()'), para poder entender e usar as informações contidas nele.
+
 
 
