cb2f157b842aab4b3d9ad2cb2a9fcd3f4e2e9257
Karen Aprigio Moraes <karenmoraes222@gmail.com>
2024-05-17 16:20:10 -0300

    revisão do conteúdo

---
+++ VueJs/material_didatico/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  const nome = "Karen Aprigio"
+  const Botao = true
+  const Imagem = "https://cdn.pixabay.com/photo/2017/11/26/15/16/smiley-2979107_1280.jpg"
+</script>
+
+<template>
+  <h1>{{ nome }}</h1>
+  <button :disabled="Botao">Entratr em contato</button>
+  <img :src="Imagem">
+</template>
+
+<style scoped>
+
+</style>
