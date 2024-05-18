2403a533936b37fe76db5512a80cc665b6f9a6c6
Karen Aprigio Moraes <karenmoraes222@gmail.com>
2024-05-18 00:29:43 -0300

    revisão do conteúdo

--- VueJs/material_didatico/src/App.vue
+++ VueJs/material_didatico/src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { reactive } from 'vue';
+
   const nome = "Karen Aprigio"
   const Botao = true
   const Emoji = "https://cdn.pixabay.com/photo/2017/11/26/15/16/smiley-2979107_1280.jpg"
@@ -8,12 +10,31 @@
 
   const Autorizado = true;
 
+  // let contador = 0
+
+  const Estado = reactive({
+    contador: 0,
+    email: ''
+  })
+
+  function incrementar() {
+    Estado.contador++;
+  }
+
+  function decrementar() {
+    Estado.contador--;
+  }
+
+  function Alterar (e) {
+    Estado.email= e.target.value
+  }
+
 </script>
 
 <template>
   <div class="container">
     <h1>{{ nome }}</h1>
-    <button :disabled="Botao">Entratr em contato</button>
+    <button :disabled="Botao" id="botao">Entratr em contato</button>
 
     <img v-if="gostaEmoji" :src="Emoji">
     <img v-else-if="gostaCafe" :src="Cafe">
@@ -23,6 +44,23 @@
     <h3 v-if="Autorizado">Bem- vinda</h3>
     <h4 v-else>Não possui acesso</h4>
   </div>
+
+  <br />
+    <hr />
+
+    {{ Estado.contador }}
+
+    <button @click="incrementar" type="button">+</button>
+    <button @click="decrementar" type="button">-</button>
+
+  <br />
+    <hr />
+
+    {{ Estado.email }}
+    <input type="email" @keyup="Alterar">
+
+    <!-- <input type="email" @keyup="evento => Estado.email = evento.target.value"> -->
+
 </template>
 
 <style scoped>
@@ -40,7 +78,7 @@
   align-items: center;
 }
 
-  button {
+  #botao {
     margin-top: 16px;
     padding: 20px;
     background-color: blue;
@@ -53,4 +91,5 @@
     margin-top: 12px;
     width: 40%;
   }
+
 </style>
